70c9d0213862243b92d75a0396ecfcc581edd7b8
tom <tomasz.dudzik.it@gmail.com>
2024-12-21 18:47:52 +0100

    Day 15 part2

--- src/main/java/Day15Java.java
+++ src/main/java/Day15Java.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 public class Day15Java {
 
-
-    private static final boolean IS_PART_2 = true;
-
     private static class Point {
         int x;
         int y;
@@ -14,6 +11,14 @@ public class Day15Java {
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        public Point getPointAtLeft() {
+            return new Point(x - 1, y);
+        }
+
+        public Point getPointAtRight() {
+            return new Point(x + 1, y);
         }
     }
 
@@ -25,9 +30,13 @@ public class Day15Java {
             this.dx = dx;
             this.dy = dy;
         }
+
+        public boolean isVertical() {
+            return dx == 0;
+        }
     }
 
-    public long part12(List<String> input, char[] moves, boolean b) {
+    public long part1(List<String> input, char[] moves) {
         char[][] map = new char[input.size()][input.get(0).length()];
         for (int i = 0; i < input.size(); i++) {
             map[i] = input.get(i).toCharArray();
@@ -39,20 +48,7 @@ public class Day15Java {
             Point position = findCurrentPosition(map);
             System.out.println("Move: " + move);
             Vector vector = translateMove(move);
-            Point nextPosition = new Point(position.x + vector.dx, position.y + vector.dy);
-
-            if (map[nextPosition.y][nextPosition.x] == '#') {
-                System.out.println("Hit wall");
-                continue;
-            } else if (map[nextPosition.y][nextPosition.x] == '.') {
-                makeMove(map, position, vector);
-            } else if (map[nextPosition.y][nextPosition.x] == 'O') {
-                boolean isMoved = makeMoveRecursive(map, nextPosition, vector);
-                if (isMoved) {
-                    makeMove(map, position, vector);
-                }
-            }
-
+            makeMoveRecursive(map, position, vector);
             //printMap(map);
         }
 
@@ -67,6 +63,189 @@ public class Day15Java {
 
         return sum;
     }
+
+    public long part2(List<String> inputOrg, char[] moves) {
+        char[][] map = new char[inputOrg.size()][inputOrg.get(0).length()*2];
+        for(int i = 0; i < inputOrg.size(); i++) {
+            String line = inputOrg.get(i);
+            for(int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                map[i][j*2] = c == 'O' ? '[' : c;
+                map[i][j*2+1] = c == '@' ? '.' : c == 'O' ? ']' : c;
+            }
+        }
+
+        int progress = 0;
+        for (char move : moves) {
+            Point position = findCurrentPosition(map);
+            System.out.println("Move: " + move);
+            Vector vector = translateMove(move);
+            if (!vector.isVertical())
+                makeMoveRecursiveHorizontally(map, position, vector);
+            else {
+                boolean isPossibleToMove = makeMoveRecursiveVerticallyByRobot(map, position, vector, false);
+                if (isPossibleToMove) {
+                    makeMoveRecursiveVerticallyByRobot(map, position, vector, true);
+                }
+            }
+
+
+            //printMap(map);
+            progress++;
+            System.out.println("Progress: " + (double)progress/moves.length * 100.0 + "%");
+        }
+
+        printMap(map);
+
+
+        long sum = 0;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                if (map[y][x] == '[') {
+                    sum += (100L * y + x);
+                }
+            }
+        }
+        return sum;
+    }
+
+    private void makeMove2(char[][] map, Point position, Vector vector, boolean shouldMove) {
+        if (!shouldMove) {
+            return;
+        }
+
+        char object = map[position.y][position.x];
+        map[position.y + vector.dy][position.x + vector.dx] = object;
+        map[position.y][position.x] = '.';
+    }
+
+    /*
+    shouldMove - if false it will only check if it is possible to move
+     */
+    private boolean makeMoveRecursiveVerticallyByBox(char[][] map, Point position, Vector vector, boolean shouldMove) {
+        Point nextPositionLeft = new Point(position.x + vector.dx, position.y + vector.dy);
+        Point nextPositionRight = nextPositionLeft.getPointAtRight();
+        char symbolAtNextPositionLeft = map[nextPositionLeft.y][nextPositionLeft.x];
+        char symbolAtNextPositionRight = map[nextPositionRight.y][nextPositionRight.x];
+        if (symbolAtNextPositionLeft == '#' || symbolAtNextPositionRight == '#') {
+            return false;
+        }
+        if (symbolAtNextPositionLeft == '.' && symbolAtNextPositionRight == '.') {
+            makeMove2(map, position, vector, shouldMove);
+            makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+            return true;
+        }
+        if (symbolAtNextPositionLeft == ']' && symbolAtNextPositionRight == '[') {
+            boolean isMovedLeft = makeMoveRecursiveVerticallyByBox(map, nextPositionLeft.getPointAtLeft(), vector, shouldMove);
+            boolean isMovedRight = makeMoveRecursiveVerticallyByBox(map, nextPositionRight, vector,shouldMove );
+            if (isMovedLeft && isMovedRight) {
+                makeMove2(map, position, vector, shouldMove);
+                makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (symbolAtNextPositionLeft == '[') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPositionLeft, vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (symbolAtNextPositionLeft == ']') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPositionLeft.getPointAtLeft(), vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (symbolAtNextPositionRight == '[') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPositionRight, vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (symbolAtNextPositionRight == ']') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPositionRight.getPointAtLeft(), vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                makeMove2(map, position.getPointAtRight(), vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid object: " + map[nextPositionLeft.y][nextPositionLeft.x]);
+    }
+
+    private boolean makeMoveRecursiveVerticallyByRobot(char[][] map, Point position, Vector vector, boolean shouldMove) {
+        Point nextPosition = new Point(position.x + vector.dx, position.y + vector.dy);
+        char symbolAtNextPosition = map[nextPosition.y][nextPosition.x];
+        if (symbolAtNextPosition == '#') {
+            return false;
+        }
+        if (symbolAtNextPosition == '.') {
+            makeMove2(map, position, vector, shouldMove);
+            return true;
+        }
+        if (symbolAtNextPosition == '[') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPosition, vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (symbolAtNextPosition == ']') {
+            boolean isMoved = makeMoveRecursiveVerticallyByBox(map, nextPosition.getPointAtLeft(), vector, shouldMove);
+            if (isMoved) {
+                makeMove2(map, position, vector, shouldMove);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid object: " + map[nextPosition.y][nextPosition.x]);
+    }
+
+    private boolean makeMoveRecursiveHorizontally(char[][] map, Point position, Vector vector) {
+        Point nextPosition = new Point(position.x + vector.dx, position.y + vector.dy);
+        char symbolAtNextPosition = map[nextPosition.y][nextPosition.x];
+        if (symbolAtNextPosition == '#') {
+            return false;
+        }
+        if (symbolAtNextPosition == '.') {
+            makeMove2(map, position, vector, true);
+            return true;
+        }
+        if (symbolAtNextPosition == '[' || symbolAtNextPosition == ']') {
+            boolean isMoved = makeMoveRecursiveHorizontally(map, nextPosition, vector);
+            if (isMoved) {
+                makeMove2(map, position, vector, true);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid object: " + map[nextPosition.y][nextPosition.x]);
+    }
+
+
 
     private Point findCurrentPosition(char[][] map) {
         for (int i = 0; i < map.length; i++) {
--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,10 +1,10 @@
 package com.xtb
 
-import Day14Java
 import Day15Java
 
 fun main() {
     val input = readInput("day15_1")
     val moves = readInput("day15_2")
-    Day15Java().part12(input, moves.joinToString(separator = "").toCharArray(), true).println()
+    // Day15Java().part1(input, moves.joinToString(separator = "").toCharArray()).println()
+    Day15Java().part2(input, moves.joinToString(separator = "").toCharArray()).println()
 }
